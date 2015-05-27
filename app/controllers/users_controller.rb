cd5bfb8dcce2faa048378149209e7006dbb3a205
Chris Garvin <chris.garvin@me.com>
2015-05-27 15:34:24 -0700

    installed bcrypt gem, added some styling, and initialized basic post index layout for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
   def index
+
+    @users = User.all
+
   end
 
   def show
+
+    @user = User.find(params[:id])
+
   end
 
   def new
+
+    @user = User.new
+
   end
 
   def create
+
+    @user = User.new(params.require(:user).permit(:handle, :fname, :lname, :email,
+     :password))
+    # session[:user_id] = @user.id
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'posts#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
